71edef42a9bc0ae3a8e627d08299a1c9c84162a4
Semih Raif Gürel <semihraifgurel@icloud.com>
2021-05-14 17:30:30 +0300

    name update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import useEventListener from "./hooks/useEventListener";
 
-export default function useEventOutSide(ref, eventName, handler) {
+export default function useEventOutside(ref, eventName, handler) {
     const checkIt = (event) => {
         if (ref.current && !ref.current.contains(event.target)) {
             handler();
